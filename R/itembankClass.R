5070526fdd9338dd9f5bb45e3395f4c492dc1723
Karel-Kroeze <k.a.kroeze@student.utwente.nl>
2015-04-13 11:39:54 +0200

    - move createTestBank into main package

--- R/itembankClass.R
+++ R/itembankClass.R
@@ -80,3 +80,57 @@ subset.ShadowCAT.items <- function(items, subset) {
   
   return(out)
 }
+
+#' Create a test Itembank
+#' 
+#' Quick and simple itembanks for testing purposes.
+#' @param model
+#' @param K
+#' @param Q
+#' @param M
+#' @return ShadowCAT.itembank
+#' @export
+createTestBank <- function(model, K = 50, Q = 1, M = 4, between = FALSE){
+  # 3PLM is dichotomous by definition
+  if (model == "3PLM") M <- 1 
+  
+  # make sure the number of items is divisible by the number of dimensions
+  if (between) K <- ceiling(K/Q) * Q 
+  
+  # set up alpha, very rough uniform from .3 to 1.5
+  alpha <- matrix(runif(K * Q, .3, 1.5), K, Q)
+  
+  # if between, force items to load on one dimension each.
+  if (between){
+    set = K / Q
+    for (i in 1:Q){
+      alpha[((i-1)*set+1):(i*set), (1:Q)[-i]] <- 0    
+    }
+  }
+  
+  # spread polytomous items cats -2 to +2.
+  spread <- seq(-2,2,length.out=M)
+  
+  # base loading for items
+  beta <- matrix(rnorm(K), K, 1)
+  
+  # apply spread for polytomous, betas are strictly monotously increasing because the spread is.
+  # apply transposes the matrix...
+  if (M > 1) beta <- t(apply(beta, 1, function(x) x + spread))
+  
+  # reparameterize GPCM
+  if (model == "GPCM") {
+    # make betas
+    eta <- beta
+    for (i in 1:M) {
+      # rolling sum, apply over all items
+      beta[,i] <- apply(eta[,1:i, drop=FALSE], 1, sum)
+    }
+  }
+  
+  # create Itembank object
+  items <- initItembank(model, alpha, beta, silent = TRUE)  
+  
+  # return
+  return(invisible(items))
+}
