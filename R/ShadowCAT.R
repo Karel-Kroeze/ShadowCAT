671cd216480b85c66f1822ffaa4ef74beea9507e
Karel-Kroeze <k.a.kroeze@student.utwente.nl>
2015-04-14 16:46:30 +0200

    - provide some intermediate output from ShadowCAT simulations, if requested. - revamp and get ready simulation scheme.

--- R/ShadowCAT.R
+++ R/ShadowCAT.R
@@ -52,8 +52,9 @@ next_item <- function(person, test) {
 #' @param test
 #' @return person
 #' @export
-ShadowCAT <- function(person, test) {
+ShadowCAT <- function(person, test, verbose = FALSE) {
   ## Start CAT
+  if (verbose > 0) cat("\n")
   while(! stop_test(person, test)) {
     # respones hack to always give 1,0,1,0 etc response pattern.
     # person$responses <- rep(c(1,0), length.out = length(person$responses))
@@ -61,7 +62,9 @@ ShadowCAT <- function(person, test) {
     
     person <- answer(person, test, next_item(person, test))
     if (length(person$responses) > test$start$n) person <- estimate(person, test, check.analyticals = FALSE)
+    if (verbose > 1) cat("\r", paste0(round(person$estimate, 2), collapse = ', '), " | ", paste0(round(diag(attr(person$estimate,'variance')), 2), collapse = ', '), '         ')
   }
   
+  if (verbose > 0) cat("\r", paste0(round(person$estimate, 2), collapse = ', '), " | ", paste0(round(diag(attr(person$estimate,'variance')), 2), collapse = ', '), '         ', "\n")
   return(person)
 }
--- tests/##simulations.R
+++ tests/##simulations.R
@@ -6,30 +6,35 @@ simCAT <- function(n = 10,
                    location = getwd(),
                    items = createTestBank("GPCM"),
                    test = initTest(items),
-                   theta0 = list(mean = rep(0,items$Q),
+                   fixed_theta = NULL,
+                   theta0 = list(mean = rep(0, items$Q),
                                  covar = diag(items$Q)),
                    prior = theta0)
-                   
+  
 {
+  cat("\n", items$model, test$selection, test$estimator, fixed_theta)
+  
   if (store) { 
     dir <- paste0(location,"/simulations/",paste(items$model, test$selection, test$estimator, sep="-"),"/",make.names(Sys.time()))
-    cat("Storing in", dir, "...\n\n")
+    cat(" - Storing in ", location, "/simulations/", sep='')
     dir.create(dir, TRUE, TRUE)
   }
   
+  cat("\n")
   results <- list()
-  thetas <- estimates <- matrix(NA, n, test$items$Q)
+  thetas <- estimates <- variances <- matrix(NA, n, test$items$Q)
   
   start.time <- Sys.time()
   
   for (i in 1:n) {
-    # draw a theta
-    theta = rmvnorm(1, theta0$mean, theta0$covar)
-
+    # draw a theta, if not specified (draws from prior, which means the prior is always correct - slightly cheating) (not used in reported simulations)
+    if (is.null(fixed_theta)) theta <- rmvnorm(1, theta0$mean, theta0$covar)
+    else theta <- fixed_theta
     # run the cat
-    results[[i]] <- person <- ShadowCAT(initPerson(items, theta = theta, prior = prior$covar), test)
+    results[[i]] <- person <- ShadowCAT(initPerson(items, theta = theta, prior = prior$covar), test, FALSE)
     thetas[i,] <- theta
     estimates[i,] <- person$estimate
+    variances[i,] <- diag( attr(person$estimate, 'variance') )
     
     if (store) save(person, file = paste0(dir, "/", i, '.CAT'))
     
@@ -37,30 +42,58 @@ simCAT <- function(n = 10,
     if (i > 2) {
       time.spent <- difftime(Sys.time(), start.time, units = 'secs')
       time.left <- (time.spent / i) * (n - i)
+      time.spent <- format(.POSIXct(time.spent, tz = "GMT"), "%H:%M:%S")
       time.string <- format(.POSIXct(time.left, tz = "GMT"), "%H:%M:%S")
     } else {
+      time.spent <- 'unknown'
       time.string <- 'unknown'
     }
-    cat("\r", i, "of", n, ", deviation: ", round(person$estimate - theta, 2), ", estimated time left: ", format(time.string, format = "%T"), "                 ")
+    cat("\r", i, " of ", n, ", deviation: ", paste(round(person$estimate - theta, 2), collapse = ' '), ", time spent: ", format(time.spent, format="%T"), ", time left: ", format(time.string, format = "%T"), "                 ",sep='')
     
   }
   
   deviations <- estimates - thetas
+  bias <- colMeans(deviations)
+  rmse <- sqrt(colMeans(deviations**2))
+  mean_var <- colMeans(variances)
+  SE_var <- apply(variances, 2, sd)
   
-  cat("\n\nBias;\n")
-  print(bias <- colMeans(deviations))
+  summary <- data.frame(bias = bias, RMSE = rmse, mean_var = mean_var, SE_var = SE_var)
+  cat('\n')
+  print(round(summary,2))
   
-  cat("\nMSE;\n")
-  print(rmse <- colMeans(deviations**2))
-  
-  if (store) save(thetas, estimates, deviations, bias, rmse, test, results, file = paste0(dir, "/!RESULTS!"))
+  if (store) save(thetas, estimates, deviations, bias, rmse, mean_var, SE_var, test, results, file = paste0(dir, "/!RESULTS!"))
+  if (store) save(summary, file = paste0(dir, "/!SUMMARY!"))
   return(invisible(results))
 }
 
 require(ShadowCAT)
 models = c("GPCM", "GRM", "SM")
+priors = list(matrix(c(1, .4,
+                            .4, 1), 2, 2),
+                   matrix(c(1, .8,
+                            .8, 1), 2, 2))
+
+theta_grid = list(NULL, c(-3,-3), c(-2,-2), c(-1,-1), c(0,0), c(1,1), c(2,2), c(3,3))
+n = 100
+K = 200
+Q = 2
+between = FALSE
 for (model in models) {
-  items <- createTestBank(model, K = 100, Q = 2, between = FALSE)
-  test <- initTest(items, estimator = "MAP", selection = "MI", objective = "PD")
-  simCAT(1000, items = items, test = test, store = TRUE)
+    items <- createTestBank(model, K, Q, 7, between)
+    test_segall <- initTest(items, estimator = "MAP", selection = "MI", objective = "PD")
+    test_shadow <- initTest(items, estimator = "MAP", selection = "Shadow", objective = "PEKL")
+    for (j in 1:length(theta_grid)) {
+      for (prior in priors){
+      simCAT(n, items = items, test = test_segall, fixed_theta = theta_grid[[j]], theta0 = list(mean = c(0,0), covar = prior), store = TRUE)
+      #simCAT(n, items = items, test = test_shadow, fixed_theta = theta_grid[[j]], theta0 = list(mean = c(0,0), prior = prior), store = TRUE)
+    }
+  }
 }
+
+# fetch all results
+# result_files <- list.files(path = paste0(getwd(),"/simulations"), pattern = '!RESULTS', all.files = FALSE,
+#                            full.names = TRUE, recursive = TRUE,
+#                            ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+# 
+# result_files
