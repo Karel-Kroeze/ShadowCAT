71ec6d92f67aef82543ae6aa199c7c6ce22df581
RivkadeVries <r.m.de.vries@umcg.nl>
2015-09-14 13:36:19 +0200

    PEKL function restructured

--- R/get_posterior_expected_kl_information.R
+++ R/get_posterior_expected_kl_information.R
@@ -16,41 +16,51 @@
 #' @return Vector with PEKL information for each item.
 #' @export
 PEKL <- function(test, person, theta_range = -3:3) {
-  # collect EAP estimate
-  if (test$estimator == "EAP") {
-    theta <- person$estimate
-  } else {
-    test$estimator <- "EAP"
-    theta <- estimate(person, test)$estimate
+  result <- function() {
+    # we'll perform a very basic integration over the theta range
+    # expand the grid for multidimensional models (number of calculations will be length(theta_range)**Q, which can still get quite high for high dimensionalities.)
+    grid <- expand.grid(rep(list(theta_range), test$items$Q))
+    
+    # sum over values of the grid
+    rowSums(apply(grid, 1, KLB, theta0 = get_theta_estimate(), test = test, person = person))
   }
   
-  # we'll perform a very basic integration over the theta range
-  # expand the grid for multidimensional models (number of calculations will be length(theta_range)**Q, which can still get quite high for high dimensionalities.)
-  grid <- expand.grid(rep(list(theta_range), test$items$Q))
-
-  # sum over values of the grid
-  info <- rowSums(apply(grid, 1, KLB, theta0 = theta, test = test, person = person))
+  get_theta_estimate <- function() {
+    # collect EAP estimate
+    if (test$estimator == "EAP") {
+      person$estimate
+    } 
+    else {
+      test$estimator <- "EAP"
+      estimate(person, test)$estimate
+    }
+  }
   
-  return(info)
+  #' Kullback Leibler Divergence for given items and pairs of thetas x posterior density.
+  #' theta0 is theta estimated with EAP
+  #' returns vector containing information for each yet available item
+  KLB <- function(theta, theta0, test, person){
+    # TODO: wrap this into PEKL, do not recompute P0 for each theta (considering it is constant for the current posterior).
+    available_items <- subset(test$items, person$available)
+    administered_items <- subset(test$items, person$administered)
+    P <- prob(test = test, theta = theta, items = available_items)$P
+    P0 <- prob(test = test, theta = theta0, items = available_items)$P
+    LL <- prob(test = test, theta = theta, person = person, items = administered_items, deriv = TRUE)$LL
+    
+    rowSums(P0 * (log(P0) - log(P)), na.rm = TRUE) * exp(LL)
+  }
+  
+  validate <- function() {
+    if (is.null(test))
+      add_error("test", "is missing")
+    if (is.null(person)) 
+      add_error("person", "is missing")
+  }
+  
+  invalid_result <- function() {
+    list(errors = errors())
+  }
+  
+  validate_and_run()
 }
 
-#' Kullback Leibler Divergence for given items and pairs of thetas x posterior density.
-#' 
-#' @param theta
-#' @param theta0
-#' @param items
-#' @param test
-#' @param person
-#' @return vector info
-KLB <- function(theta, theta0, test, person){
-  # TODO: wrap this into PEKL, do not recompute P0 for each theta (considering it is constant for the current posterior).
-  available_items <- subset(test$items, person$available)
-  administered_items <- subset(test$items, person$administered)
-  P <- prob(test = test, theta = theta, items = available_items)$P
-  P0 <- prob(test = test, theta = theta0, items = available_items)$P
-  LL <- prob(test = test, theta = theta, person = person, items = administered_items, deriv = TRUE)$LL
-  
-  out <- rowSums(P0 * (log(P0) - log(P)), na.rm = TRUE) * exp(LL)
-  
-  return(out)
-}
