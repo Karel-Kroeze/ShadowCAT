cca09ed4ba183950246c7cc4a743606fc9534d65
RivkadeVries <r.m.de.vries@umcg.nl>
2015-09-15 17:14:15 +0200

    Shadow() restructured

--- R/get_item_index_max_information_constrained.R
+++ R/get_item_index_max_information_constrained.R
@@ -11,28 +11,42 @@
 #' @return integer item index of item with maximum information within constraints
 #' @importFrom lpSolve lp
 Shadow <- function(test, person, item_information) {
-  # produce a small warning about Shadow Testing without constraints.
-  if (is.null(test$constraints)) stop("No constraints matrix given. 
-                                      Shadow Testing without constraints is equivalent to maximum information selection, but with more overhead.")
+  result <- function() {
+    # user created constraints are combined with constraint to select all administered items.
+    # RM: I have removed the administered_binair = length(responses) and length = nr_items restrictions; 
+    # I think the first one is not useful (it is already accounted for in the objective function) and may cause problems, the latter one always causes problems since it
+    # does not allow variables (Xi) to be equal to 0; i.e., the whole solution vector can only be one or zero (= no solution)
+    solution <- lp(direction = 'max',
+                   objective.in = item_information,
+                   const.mat = as.matrix(test$constraints$lp_chars)[, -1],
+                   const.dir = test$constraints$constraints$op[-1],
+                   const.rhs = test$constraints$constraints$target[-1],
+                   all.bin = TRUE,
+                   transpose.constraints = FALSE)$solution
+    
+    # get items from the pool that have the max value from the solution set.
+    which(item_information == max(item_information[as.logical(solution)]))
+    
+    # since it is theoretically possible that items not in the set share this value, cull them.
+    # RM administered items have a value of 0, so this won't happen
+    # if (length(out) > 1) out <- intersect(out, person$available)
+  }
   
-  # user created constraints are combined with constraint to select all administered items.
-  # RM: I have removed the administered_binair = length(responses) and length = nr_items restrictions; 
-  # I think the first one is not useful (it is already accounted for in the objective function) and may cause problems, the latter one always causes problems since it
-  # does not allow variables (Xi) to be equal to 0; i.e., the whole solution vector can only be one or zero (= no solution)
-  solution <- lp(direction = 'max',
-                 objective.in = item_information,
-                 const.mat = as.matrix(test$constraints$lp_chars)[, -1],
-                 const.dir = test$constraints$constraints$op[-1],
-                 const.rhs = test$constraints$constraints$target[-1],
-                 all.bin = TRUE,
-                 transpose.constraints = FALSE)$solution
+  validate <- function() {
+    # RM will improve this if statement later
+    if (ncol(as.matrix(test$constraints$lp_chars)[, -1]) == 0)
+      add_error("constraints", "matrix not given. Shadow Testing without constraints is equivalent to maximum information selection, but with more overhead.")
+    if (is.null(test))
+      add_error("test", "is missing")
+    if (is.null(person))
+      add_error("person", "is missing")
+    if (is.null(item_information))
+      add_error("item_information", "is missing")
+  }
   
-  # get items from the pool that have the max value from the solution set.
-  out <- which(item_information == max(item_information[as.logical(solution)]))
+  invalid_result <- function() {
+    list(errors = errors())
+  }
   
-  # since it is theoretically possible that items not in the set share this value, cull them.
-  if (length(out) > 1) out <- intersect(out, person$available)
-  
-  # return index
-  return(out)
+  validate_and_run()
 }
