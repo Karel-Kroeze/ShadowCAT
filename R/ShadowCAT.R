7907c5566c2a6ff104bd77d319ee964cc4400060
RivkadeVries <r.m.de.vries@umcg.nl>
2015-10-05 15:57:01 +0200

    Added some comments to ShadowCAT

--- R/ShadowCAT.R
+++ R/ShadowCAT.R
@@ -6,17 +6,25 @@
 #' Details
 #' @param person
 #' @param test
+#' @param verbose if larger than 0, print estimate and variance of estimate after test is finished. If larger than 1, also print estimate and variance of estimate at each iteration
 #' @return person
 #' @export
 ShadowCAT <- function(person, test, verbose = FALSE) {
   ## Start CAT
   if (verbose > 0) cat("\n")
-  while(! stop_test(person, test)) {
+  while(!stop_test(person, test)) {
+    # update person with new answer
     person <- answer(person, test, next_item(person, test))
-    if (length(person$responses) > test$start$n) person <- estimate(person, test)
-    if (verbose > 1) cat("\r", paste0(round(person$estimate, 2), collapse = ', '), " | ", paste0(round(diag(attr(person$estimate,'variance')), 2), collapse = ', '), '         ')
+    
+    # if inititial items have been administered (so we are in the CAT phase), update person estimate after each newly answered item
+    if (length(person$responses) > test$start$n) 
+      person <- estimate(person, test)
+    
+    if (verbose > 1) 
+      cat("\r", paste0(round(person$estimate, 2), collapse = ', '), " | ", paste0(round(diag(attr(person$estimate,'variance')), 2), collapse = ', '), '         ')
   }
   
   if (verbose > 0) cat("\r", paste0(round(person$estimate, 2), collapse = ', '), " | ", paste0(round(diag(attr(person$estimate,'variance')), 2), collapse = ', '), '         ', "\n")
-  return(invisible(person))
+  
+  invisible(person)
 }
