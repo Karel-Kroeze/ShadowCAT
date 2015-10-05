60bdb1e1d2c5863fa62d2d4dc624e8e200715c8f
RivkadeVries <r.m.de.vries@umcg.nl>
2015-10-05 15:38:03 +0200

    next_item() restructured and more tests added

--- tests/testthat/test-next_item.R
+++ tests/testthat/test-next_item.R
@@ -265,18 +265,59 @@ test_that("number of responses is 6, number of required starting items for each 
   expect_equal(item_next, 20)
 })
 
-context("invalid input")
 
-test_that("sum nByDimension is not equal to n", {
-  initiated_test <- list(start = list(type = 'nByDimension', nByDimension = 2, n = 9),
-                         items = list(Q = 3))
+test_that("items load on all three dimension while start type is nByDimension", {
+  # create item characteristics
+  model <- '3PLM'
+  number_items <- 50
+  number_dimensions <- 3
+  number_answer_categories <- 2 # can only be 2 for 3PLM model
+  guessing <- c(rep(.1, number_items / 2), rep(.2, number_items / 2))
+  eta <- NULL # only relevant for GPCM model
   
+  alpha <- matrix(with_random_seed(2, runif)(number_items * number_dimensions, .3, 1.5), nrow = number_items, ncol = number_dimensions)
+  beta <- matrix(with_random_seed(2, rnorm)(number_items), nrow = number_items, ncol = 1)
   
+  item_characteristics_shadowcat_format <- initItembank(model = model, alpha = alpha, beta = beta, guessing = guessing, eta = eta, silent = TRUE)
+  
+  # get initiated test
+  initiated_test <- initTest(item_characteristics_shadowcat_format, 
+                             start = list(type = 'randomByDimension', nByDimension = c(2, 5, 1), n = 8), 
+                             stop = list(type = 'length', n = 30),
+                             max_n = 50, # utter maximum
+                             estimator = 'MAP',
+                             objective = 'PD',
+                             selection = 'MI',
+                             constraints = NULL,
+                             exposure = NULL,
+                             lowerBound = rep(-3, item_characteristics_shadowcat_format$Q),
+                             upperBound = rep(3, item_characteristics_shadowcat_format$Q))
+  
+  initiated_person <- list(available = c(2, 4:5, 7:15, 17:18, 20:26, 28:50),
+                           responses = c(1, 3, 6, 16, 19, 27))
+  
+  item_next <- with_random_seed(2, next_item)(initiated_person, initiated_test)
+  expect_equal(item_next, 12)
 })
 
-test_that("items load on more than 1 dimension while start type is nByDimension", {
-  
-  
+context("invalid input")
+
+test_that("sum nByDimension is not equal to n, same n for each dimension", {
+  initiated_test <- list(start = list(type = 'randomByDimension', nByDimension = 2, n = 9),
+                         items = list(Q = 3))
+  initiated_person <- list(available = c(2, 4:5, 7:15, 17:18, 20:26, 28:50),
+                           responses = c(1, 3, 6, 16, 19, 27))
+  item_next <- next_item(initiated_person, initiated_test)
+  expect_equal(item_next$errors$start, "contains inconsistent information. Total length of start phase and sum of length per dimension do not match")
+})
+
+test_that("sum nByDimension is not equal to n, different n for each dimension", {
+  initiated_test <- list(start = list(type = 'randomByDimension', nByDimension = c(2, 5, 1), n = 9),
+                         items = list(Q = 3))
+  initiated_person <- list(available = c(2, 4:5, 7:15, 17:18, 20:26, 28:50),
+                           responses = c(1, 3, 6, 16, 19, 27))
+  item_next <- next_item(initiated_person, initiated_test)
+  expect_equal(item_next$errors$start, "contains inconsistent information. Total length of start phase and sum of length per dimension do not match (n != sum(nByDimension)")
 })
 
 
