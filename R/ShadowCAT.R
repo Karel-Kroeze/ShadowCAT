6cb9a2ce0bd52bdfa7a40e329a1a5274691db936
RivkadeVries <r.m.de.vries@umcg.nl>
2015-10-05 17:20:21 +0200

    Started with new ShadowCAT function

--- R/ShadowCAT.R
+++ R/ShadowCAT.R
@@ -4,6 +4,53 @@
 #'  This is a simple wrapper to call the right methods, options should be defined in the test object.
 #'
 #' Details
+#' @param person last update of person
+#' @param test test object
+#' @return person
+#' @export
+ShadowCAT_roqua <- function(person, test) {
+  result <- function() {
+    if (!stop_test(person, test)) {
+      person <- update_person_estimate()
+      index_next_item <- next_item(person, test)
+      list(person = person,
+           index_next_item = index_next_item)
+    }
+    else {
+      list(person = person,
+           index_next_item = "stop_test")
+    }  
+  }
+  
+  # if inititial items have been administered (so we are in the CAT phase), update person estimate after each newly answered item
+  update_person_estimate <- function() {
+    if (length(person$responses) > test$start$n) 
+      estimate(person, test)
+    else
+      person
+  }
+  
+  validate <- function() {
+    if (is.null(person))
+      add_error("person", "is missing")
+    if (is.null(test))
+      add_error("test", "is missing")
+  }
+  
+  invalid_result <- function() {
+    list(errors = errors())
+  }
+  
+  validate_and_run()
+}
+
+
+#' ShadowCAT Karel Kroeze
+#' 
+#' Run test with a specified person, and a specified test. See initPerson and initTest.
+#'  This is a simple wrapper to call the right methods, options should be defined in the test object.
+#'
+#' Details
 #' @param person
 #' @param test
 #' @param verbose if larger than 0, print estimate and variance of estimate after test is finished. If larger than 1, also print estimate and variance of estimate at each iteration
@@ -29,3 +76,5 @@ ShadowCAT <- function(person, test, verbose = FALSE) {
   
   invisible(person)
 }
+
+
---
+++ tests/testthat/test-ShadowCAT_roqua.R
@@ -0,0 +1,12 @@
+# only for whithin R:
+'
+library(testthat)
+'
+
+if (FALSE) {
+make_random_seed_exist <- rnorm(1)
+
+# update person with new answer
+person <- answer(person, test, indeces = next_item(person, test))
+next_item_person <- ShadowCAT_roqua(person, test)
+}
