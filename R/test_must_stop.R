5d786000f5629a22e2941a594f6fcfeaa5ad46d2
RivkadeVries <r.m.de.vries@umcg.nl>
2015-12-03 16:24:26 +0100

    test_must_stop simplified

--- R/test_must_stop.R
+++ R/test_must_stop.R
@@ -10,16 +10,10 @@
 #' @return boolean completed
 #' @export
 test_must_stop <- function(number_responses, variance_current_estimate, max_n, stop_type, stop_variance_target = NULL) {
-  # throw stopping criteria at the test until we can make something stick
-  # max length rule, for use in combination with non-length based stopping rules (<= 0 never stops for max length!)
-  if (max_n > 0 && number_responses >= max_n) 
+  max_number_items_reached <- max_n > 0 && number_responses >= max_n
+  variance_target_reached <- stop_type == 'variance' && all(diag(variance_current_estimate) < stop_variance_target)
+  if (max_number_items_reached || variance_target_reached)
     TRUE
-  
-  # variance stop rule (target (vector of length Q with 'threshold' variances))
-  else if (stop_type == 'variance' && all(diag(variance_current_estimate) < stop_variance_target))
-    TRUE
-
-  # if nothing stuck, don't stop!
   else
     FALSE
 }
