ba6e9bc0784110bbd4a0c7311098fa4c157e7d6c
RivkadeVries <r.m.de.vries@umcg.nl>
2016-02-16 14:19:58 +0100

    Extra tests for random by dimension sampling

--- tests/testthat/test-get_next_item.R
+++ tests/testthat/test-get_next_item.R
@@ -85,7 +85,7 @@ test_that("number of responses is 4, number of required starting items 5, start 
   expect_equal(item_next, 9)
 })
 
-context("start type is random by dimension")
+context("start type is random by dimension, items ordered per dimension")
 
 model <- "3PLM"
 number_items <- 50
@@ -127,6 +127,7 @@ test_that("number of responses is 6, number of required starting items 3 per dim
 })
 
 test_that("number of responses is 0, number of required starting items for each dimension is 2, 4, 2, random by dimension", {
+  start_items <- list(type = 'random_by_dimension', n_by_dimension = c(2, 4, 2), n = 8)
   available <- 1:50
   responses <- numeric(0)
   administered <- numeric(0)
@@ -137,7 +138,6 @@ test_that("number of responses is 0, number of required starting items for each 
 
 test_that("number of responses is 6, number of required starting items for each dimension is 2, 5, 1, random by dimension", {
   start_items <- list(type = 'random_by_dimension', n_by_dimension = c(2, 5, 1), n = 8)
-  
   available <- c(2, 4:5, 7:15, 17:18, 20:26, 28:50)
   responses <- rep(1, 6)
   administered <- c(1, 3, 6, 16, 19, 27)
@@ -159,6 +159,79 @@ test_that("items load on all three dimension while start type is n_by_dimension"
   expect_equal(item_next, 12)
 })
 
+context("start type is random by dimension, items not ordered per dimension")
+
+model <- "3PLM"
+number_items <- 50
+number_dimensions <- 3
+start_items <- list(type = 'random_by_dimension', n_by_dimension = 3, n = 9) 
+estimator <- "maximum_aposteriori"
+information_summary <- "posterior_determinant"
+lp_constraints <- NULL
+lp_characters <- NULL
+estimate <- 0
+prior <- .4
+lower_bound <- -3
+upper_bound <- 3
+
+guessing <- c(rep(.1, number_items / 2), rep(.2, number_items / 2))  
+alpha <- matrix(with_random_seed(2, runif)(number_items * number_dimensions, .3, 1.5), nrow = number_items, ncol = number_dimensions)
+indeces_shuffled <- with_random_seed(2, sample)(1:50, 50)
+alpha[indeces_shuffled[1:10],2:3] <- 0
+alpha[indeces_shuffled[11:20],c(1,3)] <- 0
+alpha[indeces_shuffled[21:50],1:2] <- 0
+beta <- matrix(with_random_seed(2, rnorm)(number_items), nrow = number_items, ncol = 1)
+number_itemsteps_per_item <- number_non_missing_cells_per_row(beta)
+
+test_that("number of responses is 0, number of required starting items 3 per dimension, random by dimension", {
+  responses <- numeric(0)
+  available <- 1:50
+  administered <- numeric(0)
+  
+  item_next <- with_random_seed(2, get_next_item)(start_items, information_summary, lp_constraints, lp_characters, estimate, model, responses, prior, available, administered, number_items, number_dimensions, estimator, alpha, beta, guessing, number_itemsteps_per_item, lower_bound, upper_bound)
+  expect_equal(item_next, 8)
+})
+
+test_that("number of responses is 6, number of required starting items 3 per dimension, random by dimension", {
+  available <- c(2, 4:5, 7:15, 17:18, 20:26, 28:50)
+  responses <- rep(1, 6)
+  administered <- c(1, 3, 6, 16, 19, 27)
+  
+  item_next <- with_random_seed(2, get_next_item)(start_items, information_summary, lp_constraints, lp_characters, estimate, model, responses, prior, available, administered, number_items, number_dimensions, estimator, alpha, beta, guessing, number_itemsteps_per_item, lower_bound, upper_bound)
+  expect_equal(item_next, 11)
+})
+
+test_that("number of responses is 0, number of required starting items for each dimension is 2, 4, 2, random by dimension", {
+  start_items <- list(type = 'random_by_dimension', n_by_dimension = c(2, 4, 2), n = 8)
+  available <- 1:50
+  responses <- numeric(0)
+  administered <- numeric(0)
+  
+  item_next <- with_random_seed(2, get_next_item)(start_items, information_summary, lp_constraints, lp_characters, estimate, model, responses, prior, available, administered, number_items, number_dimensions, estimator, alpha, beta, guessing, number_itemsteps_per_item, lower_bound, upper_bound)
+  expect_equal(item_next, 8)
+})
+
+test_that("number of responses is 6, number of required starting items for each dimension is 2, 5, 1, random by dimension", {
+  start_items <- list(type = 'random_by_dimension', n_by_dimension = c(2, 5, 1), n = 8)
+  available <- c(2, 4:5, 7:15, 17:18, 20:26, 28:50)
+  responses <- rep(1, 6)
+  administered <- c(1, 3, 6, 16, 19, 27)
+  
+  item_next <- with_random_seed(2, get_next_item)(start_items, information_summary, lp_constraints, lp_characters, estimate, model, responses, prior, available, administered, number_items, number_dimensions, estimator, alpha, beta, guessing, number_itemsteps_per_item, lower_bound, upper_bound)
+  expect_equal(item_next, 15)
+})
+
+test_that("number of responses is 6, number of required starting items for each dimension is 2, 5, 1, random by dimension", {
+  start_items <- list(type = 'random_by_dimension', n_by_dimension = c(2, 5, 1), n = 8)
+  available <- c(2, 4:5, 7:14, 17:18, 20:26, 28:50)
+  responses <- rep(1, 7)
+  administered <- c(1, 3, 6, 16, 19, 27, 15)
+  
+  item_next <- with_random_seed(2, get_next_item)(start_items, information_summary, lp_constraints, lp_characters, estimate, model, responses, prior, available, administered, number_items, number_dimensions, estimator, alpha, beta, guessing, number_itemsteps_per_item, lower_bound, upper_bound)
+  expect_equal(item_next, 11)
+})
+
+
 context("invalid input")
 
 available <- c(2, 4:5, 7:15, 17:18, 20:26, 28:50)
@@ -178,5 +251,4 @@ test_that("sum n_by_dimension is not equal to n, different n for each dimension"
   item_next <- get_next_item(start_items, information_summary, lp_constraints, lp_characters, estimate, model, responses, prior, available, administered, number_items, number_dimensions, estimator, alpha, beta, guessing, number_itemsteps_per_item, lower_bound, upper_bound)
   expect_equal(item_next$errors$start, "contains inconsistent information. Total length of start phase and sum of length per dimension do not match (n != sum(n_by_dimension)")
 })
-
 
